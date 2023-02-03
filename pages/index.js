e48895c94e48a800901fdb30c545771f9fb27aa6
Ismet Glumcevic <ismet.glumcevic@gmail.com>
2023-02-03 09:30:59 +0100

    aws import

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Amplify, Auth, Hub, Logger } from 'aws-amplify';
 
-import awsmobile from '../src/aws-exports';
+import awsconfig from '../src/aws-exports';
 import { useEffect, useState } from 'react';
-Amplify.configure({ ...awsmobile, ssr: true });
+Amplify.configure({ ...awsconfig, ssr: true });
 Amplify.Logger.LOG_LEVEL = 'DEBUG'
 
 const logger = new Logger('My-Logger');
